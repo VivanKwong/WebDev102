eff627997169bc6ddbbc95a05dc18643847592f5
VivanKwong <vivianakwong@gmail.com>
2023-09-21 11:49:50 -0400

    first commit

---
+++ timetabled/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Calendar from './components/Calendar';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>Itinerary for 7 Days in Chicago</h1>
+      <h2>One week in Chicago is wonderful. If you want to get to know the city and see all the sites, then spending 7 days in Chicago is perfect!</h2>
+      <Calendar />
+    </div>
+  )
+}
+
+export default App
